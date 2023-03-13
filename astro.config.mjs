550b5dbb35d7e76c76c99a91534f388f9304503b
jordanpurinton <jordanomac@gmail.com>
2023-03-12 20:50:12 -0500

    vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import react from '@astrojs/react';
 
 // https://astro.build/config
 import image from '@astrojs/image';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react(), image()],
+	output: 'server',
+	adapter: vercel({
+		analytics: true,
+	}),
 });
